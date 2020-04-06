c9330d4caa98bf2655257b5c8bf3244c92c5f0cb
Handoyo Sutanto <handoyosutanto@gmail.com>
2020-04-05 19:49:06 -0700

    adding more model

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"github.com/LyridInc/go-sdk"
+)
+
 func main() {
 
+	fmt.Println(sdk.Hello())
+
+	sdk.Initialize("34pQkHp7drouTGB2Z0ph", "Qw2FIhoBDXoxQLEV7r670VAogA1YN0KpV5faISINxnHLRQfRPV")
 }
---
+++ model/job.go
@@ -0,0 +1,28 @@
+package model
+
+import "time"
+
+type JobStatusEnum uint
+
+const (
+	Submitting JobStatusEnum = iota
+	Submitted
+	Queued
+	Running
+	Completed
+	Error
+	Warning
+	Cancelled
+	Finished
+	TimedOut
+)
+
+type Job struct {
+	ID             string        `json:"id" binding:"required"`
+	AccountId      string        `json:"accountId" binding:"required"`
+	Type           string        `json:"type" binding:"required"`
+	Status         JobStatusEnum `json:"status" binding:"required"`
+	CreationTime   time.Time     `json:"creationTime"`
+	LastUpdateTime time.Time     `json:"lastUpdateTime"`
+	Payload        string        `json:"payload"`
+}
